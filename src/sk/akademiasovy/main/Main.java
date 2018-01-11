0b0ef8db9ce8012646675da0017c70927c3f2a4f
Daniela8888 <daniela.vecserova@akademiasovy.sk>
2018-01-11 09:58:22 +0100

    .

---
+++ src/sk/akademiasovy/main/Main.java
@@ -0,0 +1,22 @@
+package sk.akademiasovy.main;
+
+import sk.akademiasovy.geometry.Rectangle;
+import sk.akademiasovy.geometry.Square;
+
+public class Main {
+    public static void main(String[] args) {
+        Square square1 = new Square(7.5);
+        Square square2 = new Square();
+
+        System.out.println("Area of square1 is "+ square1.getArea() );
+        System.out.println("Perimeter of square1 is "+ square1.getPerimeter() );
+        System.out.println("Diagonal's length of square1 is "+ square1.getDiagonal() );
+
+        Rectangle rectangle1 = new Rectangle(18.5,11.15);
+        Rectangle rectangle2 = new Rectangle();
+
+        System.out.println("Area of rectangle1 is "+ rectangle1.getArea() );
+        System.out.println("Perimeter of rectangle1 is "+ rectangle1.getPerimeter() );
+        System.out.println("Diagonal's length of rectangle1 is "+ rectangle1.getDiagonal() );
+    }
+}
